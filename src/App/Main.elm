f2743dfa3c44319373406b5e057df8536d7a0040
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 11:39:50 +0200

    next

--- src/App/Main.elm
+++ src/App/Main.elm
@@ -1,75 +1,20 @@
 module App.Main exposing (..)
 
 import App.Model
+import App.Model.Update
+import App.Model.View
 import App.Msg
-import App.Utils.Translation
 import Browser
-import Browser.Navigation
-import Browser.QueryRouter
-import Html
 import Json.Decode
-import Platform.Extra
-import Url
 
 
 main : Program Json.Decode.Value App.Model.Model App.Msg.Msg
 main =
     Browser.application
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
+        { init = App.Model.Update.init
+        , update = App.Model.Update.update
+        , subscriptions = App.Model.Update.subscriptions
+        , view = App.Model.View.view
         , onUrlRequest = App.Msg.UrlRequested
         , onUrlChange = App.Msg.UrlChanged
         }
-
-
-
---
-
-
-init : Json.Decode.Value -> Url.Url -> Browser.Navigation.Key -> ( App.Model.Model, Cmd App.Msg.Msg )
-init _ url key =
-    ( { router = Browser.QueryRouter.init App.Model.pageFromUrl url key
-      }
-    , Cmd.none
-    )
-
-
-
---
-
-
-update : App.Msg.Msg -> App.Model.Model -> ( App.Model.Model, Cmd App.Msg.Msg )
-update msg model =
-    case msg of
-        App.Msg.NothingHappened ->
-            Platform.Extra.noOperation model
-
-        App.Msg.UrlRequested a ->
-            Browser.QueryRouter.urlRequested a model
-
-        App.Msg.UrlChanged a ->
-            Browser.QueryRouter.urlChanged App.Model.pageFromUrl a model
-
-
-
---
-
-
-subscriptions : App.Model.Model -> Sub App.Msg.Msg
-subscriptions _ =
-    Sub.none
-
-
-
---
-
-
-view : App.Model.Model -> Browser.Document App.Msg.Msg
-view _ =
-    { title = App.Utils.Translation.title
-    , body =
-        [ Html.text App.Utils.Translation.title
-        ]
-    }
